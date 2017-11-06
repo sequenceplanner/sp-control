1d68acdfd7a6133a4b3318bf5c5d4169d77ff891
Ashfaq Farooqui <ashfaq.farooqui@gmail.com>
2017-11-06 17:39:49 +0100

    Adding simple widget

---
+++ frontend/src/main/scala/spgui/widgets/robotservices/RobotServiceWidget.scala
@@ -0,0 +1,105 @@
+package spgui.widgets.robotservices
+
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.html_<^._
+
+import spgui.communication._
+
+// Import this to make SPAttributes work including json handling
+import sp.domain._
+import Logic._
+
+
+import sp.robotservices.{APIRobotServices => api}
+
+
+
+  object RobotLogServiceWidget {
+
+    case class State(pathModules: String, pathLogs: String, running:Boolean)
+
+    private class Backend($: BackendScope[Unit, State]) {
+
+      val messObs = BackendCommunication.getMessageObserver(
+        mess => {
+          val callback: Option[CallbackTo[Unit]] = mess.getBodyAs[api.Response].map {
+            case api.Finished =>
+                $.modState(s => s.copy(running = false))
+
+            case api.Started =>
+              $.modState (s =>s.copy(running = true))
+          }
+          callback.foreach(_.runNow())
+
+        },
+        api.topicResponse
+      )
+
+      def pathLogChange(s: State)(e: ReactEventFromInput) = {
+        $.modState(s=>s.copy(pathLogs = e.target.value)).runNow()
+        Callback.empty
+      }
+      def pathModChange(s: State)(e: ReactEventFromInput) = {
+        $.modState(s=>s.copy(pathModules = e.target.value)).runNow()
+        Callback.empty
+      }
+      def render(s: State) = {
+        <.div(
+          <.h2("Running? " + (if(s.running) "yes" else "no")),
+          <.br(),
+          <.input(
+            ^.value     := s.pathLogs,
+            ^.onChange ==> pathLogChange(s)
+          ),
+          <.button(
+            ^.className := "btn btn-default",
+            ^.onClick --> send(api.LoadLog(s.pathLogs)), "Load robot Logs"
+          ),
+            <.br(),
+            <.input(
+              ^.value     := s.pathModules,
+              ^.onChange ==> pathModChange(s)
+            ),
+          <.button(
+            ^.className := "btn btn-default",
+            ^.onClick --> send(api.LoadRobotModules(s.pathModules)), "Load robot modules"
+          ),
+
+          <.button(
+            ^.className := "btn btn-default",
+            ^.onClick --> send(api.PlayLogs), "play log"
+          ),
+
+          <.button(
+          ^.className := "btn btn-default",
+          ^.onClick --> send(api.Connect), "Connect"
+        )
+        )
+      }
+
+      def onUnmount() = {
+        println("Unmounting")
+        messObs.kill()
+        Callback.empty
+      }
+
+
+      def send(mess: api.Request): Callback = {
+        val h = SPHeader(from = "robotLogServiceWidget", to = api.logPlayer, reply = SPValue("robotLogServiceWidget"))
+        val json = SPMessage.make(h, mess) // *(...) is a shorthand for toSpValue(...)
+        BackendCommunication.publish(json, api.topicRequest)
+        Callback.empty
+      }
+
+
+    }
+
+
+    private val component = ScalaComponent.builder[Unit]("RobotLogServiceWidget")
+      .initialState(State( pathLogs= "/home/ashfaqf/Projects/Lisa files/from_volvo/logs/20-10-2017/logs/log-1741010_10_19_14_53", pathModules = "/home/ashfaqf/Projects/Lisa files/from_volvo/logs/20-10-2017/RobotPrograms/1741010", running = false))
+      .renderBackend[Backend]
+      .componentWillUnmount(_.backend.onUnmount())
+      .build
+
+    def apply() = spgui.SPWidget(spwb => component())
+  }
