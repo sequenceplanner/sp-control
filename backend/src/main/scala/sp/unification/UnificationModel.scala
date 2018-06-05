6d356135d46cbb1610cf9cb71e5b8efbce61e859
Martin Dahl <martin.dahl@gmail.com>
2018-06-05 17:14:26 +0200

    Model for the UR Dummy poses. Should "just work"

--- backend/src/main/scala/sp/unification/UnificationModel.scala
+++ backend/src/main/scala/sp/unification/UnificationModel.scala
@@ -8,30 +8,33 @@ import sp.drivers.ROSFlatStateDriver
 
 class URModel(n: String) extends VDHelper {
   val name = n
-  // // ur robot state
-  // dv("ur_act_pos", "unification_roscontrol/URPose1:/unification/ur_pose_unidriver/state:actPos")
-  // dv("ur_executing", "unification_roscontrol/URPose1:/unification/ur_pose_unidriver/state:executing")
-  // dv("ur_ref_pos_driver", "unification_roscontrol/URPose1:/unification/ur_pose_unidriver/state:refPos")
+  // ur robot state
+  dv("ur_act_pos", "driver", "unification_roscontrol/URPose1:/unification/ur_pose_unidriver/state:actPos")
+  dv("ur_executing", "driver", "unification_roscontrol/URPose1:/unification/ur_pose_unidriver/state:executing")
+  dv("ur_ref_pos_driver", "driver", "unification_roscontrol/URPose1:/unification/ur_pose_unidriver/state:refPos")
 
-  // // ur robot commands
-  // dv("ur_ref_pos_sp", "unification_roscontrol/URPose2:/unification/ur_pose_unidriver/cmd:refPos:250")
+  // ur robot commands
+  dv("ur_ref_pos_sp", "driver", "unification_roscontrol/URPose2:/unification/ur_pose_unidriver/cmd:refPos:250")
 
-  // // ur abilities
-  // List("URDummyPose1", "URDummyPose2", "URDummyPose3", "URDummyPose4").foreach { pose =>
-  //   a("goto"+pose, List(),
-  //     ac("pre", "true", s"ur_ref_pos_sp := '$pose'"),// s"ur_act_pos != '$pose'", s"ur_ref_pos_sp := '$pose'"),
-  //     ac("started", s"ur_ref_pos_driver == '$pose' && ur_executing"), // note that we check the driver state
-  //     ac("post", s"ur_act_pos == '$pose' && !ur_executing"),
-  //     ac("reset", "true"))
-  // }
-  // // leave all abilities unchanged except for movetopose2
-  // //  o("gotoURDummyPose2", p("pre", "ur_act_pos == 'URDummyPose1'"))
+  // ur abilities
+  List("URDummyPose1", "URDummyPose2", "URDummyPose3", "URDummyPose4").foreach { pose =>
+    a("goto"+pose, List(),
+      ac("pre", "true", s"ur_ref_pos_sp := '$pose'"),
+      ac("started", s"ur_ref_pos_driver == '$pose' && ur_executing"), // note that we check the driver state
+      ac("post", s"ur_act_pos == '$pose' && !ur_executing"),
+      ac("reset", "true"))
+  }
+  // add a sequence
+  o("gotoURDummyPose2", oc("pre", "ur_act_pos == 'URDummyPose1'"))
+  o("gotoURDummyPose3", oc("pre", "ur_act_pos == 'URDummyPose2'"))
+  o("gotoURDummyPose4", oc("pre", "ur_act_pos == 'URDummyPose3'"))
 
-  // // drivers and resources
-  // val driverID = ID.newID
-  // val driverSetup = SPAttributes("identifiers" -> driverMapper(driverID).map(_.driverIdentifier))
-  // val driver = VD.Driver("URVD", driverID, ROSFlatStateDriver.driverType, driverSetup)
-  // val resource = VD.Resource("UR", ID.newID, dthings.values.map(_.id).toSet, driverMapper(driverID).toList, SPAttributes())
+  // drivers and resources
+  driver("driver", ROSFlatStateDriver.driverType)
+  resource("resource") // blank list of things = take everything
+
+  // runner
+  r("runner", initState = Map("ur_act_pos" -> "unknown?"))
 }
 
 object URModel {
