cb665c16f0367487885f6a27f038a8f53e27646b
Jonathan Krän <jonathan.kran1@gmail.com>
2018-06-08 10:29:34 +0200

    Add utility class for formatting Maps to Json

---
+++ backend/src/main/scala/sp/FormatUtility.scala
@@ -0,0 +1,38 @@
+package sp
+
+import play.api.libs.json.{Format, JsObject}
+import sp.domain.{JSFormat, JSReads, JSWrites, SPValue}
+
+import scala.util.Try
+
+object FormatUtility {
+  def mapWrites[K, V](f: K => String)(implicit writes: JSWrites[V]): JSWrites[Map[K, V]] = xs => {
+    JsObject(xs.map { case (k, v) => f(k) -> SPValue(v) })
+  }
+
+  def mapReads[K, V](keyParser: String => Option[K])(implicit reads: JSReads[V]): JSReads[Map[K, V]] = _.validate[Map[String, SPValue]].map(
+    _.collect { case (k, v) =>
+      for (key <- keyParser(k); value <- v.to[V].toOption) yield key -> value
+    }.flatten.toMap
+  )
+
+  /**
+    *
+    * @param deserializeKey Function to turn a String key into its correct type
+    * @param serializeKey Function to serialize the key into a String
+    * @param f JSFormat for the map value type
+    * @tparam K The map key type
+    * @tparam V The map value type
+    * @return A JSFormat for Map[K, V]
+    *
+    *         Usage example:
+    *         implicit lazy val fOpenWidgetMap = mapFormat[ID, OpenWidget](k => ID.makeID(k), k => k.toString)
+    */
+  def mapFormat[K, V](deserializeKey: String => Option[K], serializeKey: K => String)(implicit f: JSFormat[V]): Format[Map[K, V]] = {
+    Format(mapReads[K, V](deserializeKey), mapWrites[K, V](serializeKey))
+  }
+
+  implicit class SPValueExtension(v: SPValue) {
+    def to[T](implicit fjs: JSReads[T]): Try[T] = Try { v.as[T] }
+  }
+}
