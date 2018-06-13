93c74944240f45845b6f03a78a8dab8aed2af8d6
aleastChs <aleast@student.chalmers.se>
2018-06-13 10:15:05 +0200

    DriverWidget: Removed old code, started with scaladoc for documenting

--- frontend/src/main/scala/spgui/widgets/VDGUI/DriverWidget.scala
+++ frontend/src/main/scala/spgui/widgets/VDGUI/DriverWidget.scala
@@ -2,7 +2,7 @@ package spgui.widgets.VDGUI
 
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
-import sp.devicehandler.{VD, APIDeviceDriver, APIVirtualDevice}
+import sp.devicehandler.{VD, APIDeviceDriver}
 import sp.domain._
 import spgui.communication._
 import spgui.components.SPWidgetElements
@@ -14,27 +14,33 @@ object DriverWidget {
   case class State(cards:  List[Card] = List())
 
   private class Backend($: BackendScope[Unit, State]) {
-
-    val deviceHandler = BackendCommunication.getMessageObserver(onDeviceMessage, APIVirtualDevice.topicResponse)
     val driverHandler = BackendCommunication.getMessageObserver(onDriverMessage, APIDeviceDriver.topicResponse)
-
-    def onDeviceMessage(mess: SPMessage) = {
-
-    }
 
     def onDriverMessage(mess: SPMessage) = {
       val callback: Option[CallbackTo[Unit]] = mess.getBodyAs[APIDeviceDriver.Response].map {
+        /**
+          * if a [[APIDeviceDriver.TheDrivers]] response is noticed
+          * add the drivers to a card
+          */
         case APIDeviceDriver.TheDrivers(drivers) => {
           println(drivers.toString)
           $.modState { s =>
             s.copy(cards = drivers.map(d => Card(d._1, d._2, d._1.id)))
           }
         }
+        /**
+          * if a [[APIDeviceDriver.TheDriver]] response is noticed
+          * add the drivers to a card if it not already exist in cards
+          */
         case APIDeviceDriver.TheDriver(d: VD.Driver, driverState: VD.DriverState) => {
           $.modState { s =>
             s.copy(cards = s.cards.filter(c => c.cardId != d.id) :+ Card(d, driverState, d.id))
           }
         }
+        /**
+          * if a [[APIDeviceDriver.DriverStateChange]] response is noticed
+          * update the driver in the cards with the help method onDriverStateChange()
+          */
         case APIDeviceDriver.DriverStateChange(name, id, state, diff) => {
           onDriverStateChange(name, id, state, diff)
         }
@@ -75,105 +81,43 @@ object DriverWidget {
       )
     }
 
-    /*
-     send driverID to VDDriverCardsWidget and expand
-     When expanded you should be able to:
-     1. Force Restart
-     2. Force Stop
-     3. Force Write
-     */
-    def renderExpansion(card: Card) = {
-      <.div(
-        //^.onClick --> onCardClick(card),
-        <.div(
-          <.button(
-            ^.className := "btn btn-default",
-            ^.onClick --> forceStop(card), "Force Stop"
-          ),
-          <.button(
-            ^.className := "btn btn-default",
-            ^.onClick --> forceRestart(card), "Force Restart"
-          ),
-          <.button(
-            ^.className := "btn btn-default",
-            ^.onClick --> forceWrite(card), "Force Write"
-          )
-        ),
-        <.div(
-          "Name:   " + card.driver.name + "\n" +
-            "ID:     " + card.driver.id + "\n" +
-            //"Online: " + (if (card.driver.driverIsOnline) "Driver Online" else "Driver Offline") + "\n" +
-            "Type:   " + card.driver.driverType + "\n" +
-            "Setup   " + card.driver.setup + "\n" +
-            renderDriverState(card)
-        )
-      )
-    }
-
-    def renderDriverState(card: Card) = {
-      // for each element in driverState (Map[String, SPValue])
-      // print String, SPValue and a box where we can change SPValue if driver is editable
-      // Later: create new driverStates
-      card.driverState.toList.map { state: (String, SPValue) =>
-        <.div(
-          state._1 + "  " + state._2.toString(),
-          <.button(
-            ^.onClick --> onEditStateClicked(card),
-            "Edit SPValue"
-          )
-        )
-      }
-    }
-
-    def onEditStateClicked(card: Card) = {
-      Callback("DriverWidget: Edit State-Button clicked") // dummy
-    }
-
     /**********ACTIONS**********/
+    /* "DriverWidget: Edit State-Button clicked") // dummy
+       TODO: Should edit one state of the driver */
+    def onEditStateClicked(card: Card) = ???
 
-    def onCardClick(card: Card)= {
-      <.div(renderExpansion(card))
-      // send to widget api that card is clicked
-      // handle in BackendComm.MessageObserver that the card should expand/contract
-      //Callback("DriverWidget: Card has been clicked") // dummy
-
-      Callback.empty
-    }
-
-
-
-    /**********CALLBACKS**********/
+    /**********BACKEND CALLS**********/
     /*
 
-    // Todo: Test that the force does what we desire
+        Todo: Test that the force does what we desire
         should return a message to circuit or backend
      */
-    def forceWrite(card: Card) = {
+    def forceWrite(card: Card) = ???
+    /*{
       // callback to backend to write new SPValues to the driver
       sendToDeviceDriver(APIDeviceDriver.DriverCommand(card.driver.id, card.driverState))
       Callback("DriverWidget: Force the driver to write over past state") // dummy
-    }
+    }*/
     /*
-     force the driver to stop
+      TODO: force the driver to go offline/terminate
      */
-    def forceStop(card: Card) = {
+    def forceStop(card: Card) = ???
+    /*{
       // callback to backend to stop the driver
       sendToDeviceDriver(APIDeviceDriver.TerminateDriver(card.driver.id))
-
-      //Callback("DriverWidget: Force the driver to stop") // dummy
-    }
+    }*/
     /*
-     force the driver to restart
+       TODO: force the driver to restart
      */
-    def forceRestart(card: Card) = {
+    def forceRestart(card: Card) = ???
+    /*{
       // callback to backend to restart the driver
       sendToDeviceDriver(APIDeviceDriver.SetUpDeviceDriver(card.driver))
       Callback("DriverWidget: Force the driver to restart") // dummy
-    }
+    }*/
 
     def onUnmount() = {
       println("DriverWidget Unmouting")
-      deviceHandler.kill()
       driverHandler.kill()
       Callback.empty
     }
