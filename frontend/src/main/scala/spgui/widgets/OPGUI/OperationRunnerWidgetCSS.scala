abf0710b10000b8465999769c1a8ec6571d28b25
martinpettersson <laxenmeflaxen+git@gmail.com>
2018-06-15 12:18:58 +0200

    some generic elements changed to use SPWidgetElements

--- frontend/src/main/scala/spgui/widgets/OPGUI/OperationRunnerWidgetCSS.scala
+++ frontend/src/main/scala/spgui/widgets/OPGUI/OperationRunnerWidgetCSS.scala
@@ -32,7 +32,8 @@ object OperationRunnerWidgetCSS extends StyleSheet.Inline {
     boxShadow:= "0px 1px 5px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)",
     display.flex,
     flexGrow(1),
-    overflow.auto
+    overflow.auto,
+    position.relative
   )
 
   val cardCollapsed = style(
