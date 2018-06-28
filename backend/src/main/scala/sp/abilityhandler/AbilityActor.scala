b498ee60545413c6d8a6fc6e5d6442d54c4c69b3
Kristofer Bengtsson <kristofer@sekvensa.se>
2018-06-28 15:08:14 +0200

    A fix to not evaluate an empty state in the ability

--- backend/src/main/scala/sp/abilityhandler/AbilityActor.scala
+++ backend/src/main/scala/sp/abilityhandler/AbilityActor.scala
@@ -182,51 +182,52 @@ trait AbilityActorLogic extends AbilityLogic {
     * @return (String, SPState), Where the string states the new state, or empty if
     *         the state was not changed. And the state is the new updated state
     */
-  def updateState(theState: SPState, cmd: String): (String, SPState) = state match {
-    case x if cmd == "start" &&
-      (x == EnabledTag || x == NotEnabledTag || x == UnavailableTag) &&
-      ability.preCondition.eval(theState) =>
+  def updateState(theState: SPState, cmd: String): (String, SPState) = {
+    state match {
+      case x if cmd == "start" &&
+        (x == EnabledTag || x == NotEnabledTag || (x == UnavailableTag && theState.state.nonEmpty)) &&
+        ability.preCondition.eval(theState) =>
 
-      if (ability.started.eval(theState)) // skipping starting for simple abilities
-        (Executing, ability.started.next(ability.preCondition.next(theState)))
-      else
-        (Starting, ability.preCondition.next(theState))
+        if (ability.started.eval(theState)) // skipping starting for simple abilities
+          (Executing, ability.started.next(ability.preCondition.next(theState)))
+        else
+          (Starting, ability.preCondition.next(theState))
 
-    case x if cmd == "reset" && !(x == ForcedResetTag || x == UnavailableTag || x == EnabledTag || x == NotEnabledTag) =>
-      (ForcedReset, theState)
+      case x if cmd == "reset" && !(x == ForcedResetTag || x == UnavailableTag || x == EnabledTag || x == NotEnabledTag) =>
+        (ForcedReset, theState)
 
-    case x if x == ForcedResetTag =>
-      val updS =  ability.resetCondition.next(theState)
-      (checkEnabled(updS), updS)
+      case x if x == ForcedResetTag =>
+        val updS =  ability.resetCondition.next(theState)
+        (checkEnabled(updS), updS)
 
-    case x if x == UnavailableTag && theState.state.nonEmpty =>
-      (NotEnabled, theState)
+      case x if x == UnavailableTag && theState.state.nonEmpty =>
+        (NotEnabled, theState)
 
-    case x if x != ExecutingTag && syncedExecution(ability) && ability.started.eval(theState) =>
-      (Executing, theState)
-    case x if x != FinishedTag && syncedFinished(ability) && ability.postCondition.eval(theState) =>
-      (Finished, theState)
+      case x if x != ExecutingTag && syncedExecution(ability) && ability.started.eval(theState) =>
+        (Executing, theState)
+      case x if x != FinishedTag && syncedFinished(ability) && ability.postCondition.eval(theState) =>
+        (Finished, theState)
 
-    case x if x == NotEnabledTag && ability.preCondition.eval(theState) =>
-      (Enabled, theState)
-    case x if x == EnabledTag && !ability.preCondition.eval(theState) =>
-      (NotEnabled, theState)
-
-
-    case x if x == StartingTag && ability.started.eval(theState) =>
-      (Executing, ability.started.next(theState))
-    case x if x == ExecutingTag && ability.postCondition.eval(theState) =>
-      (Finished, ability.postCondition.next(theState))
-    case x if x == FinishedTag && ability.resetCondition.eval(theState) =>
-      val updS = ability.resetCondition.next(theState)
-      (checkEnabled(updS), updS)
+      case x if x == NotEnabledTag && ability.preCondition.eval(theState) =>
+        (Enabled, theState)
+      case x if x == EnabledTag && !ability.preCondition.eval(theState) =>
+        (NotEnabled, theState)
 
 
-    //case x if x == failed => // not yet used. fail if driver fails
+      case x if x == StartingTag && ability.started.eval(theState) =>
+        (Executing, ability.started.next(theState))
+      case x if x == ExecutingTag && ability.postCondition.eval(theState) =>
+        (Finished, ability.postCondition.next(theState))
+      case x if x == FinishedTag && ability.resetCondition.eval(theState) =>
+        val updS = ability.resetCondition.next(theState)
+        (checkEnabled(updS), updS)
 
-    case _ => ("", theState) // No change
 
-  }
+      //case x if x == failed => // not yet used. fail if driver fails
+
+      case _ => ("", theState) // No change
+
+  }}
 
   def checkEnabled(tS: SPState): String = if (ability.preCondition.eval(tS)) Enabled else NotEnabled
 
