f5293f25c43d7d87eece6eb37ce561cd384ff920
Kristofer Bengtsson <kristofer@sekvensa.se>
2018-10-01 16:54:59 +0200

    Updated the runner with some new methods

--- backend/src/main/scala/sp/runners/RunnerPipeline.scala
+++ backend/src/main/scala/sp/runners/RunnerPipeline.scala
@@ -27,7 +27,7 @@ case class RunnerPipeline(operations: List[Operation],
     val ticker = includeTicker.map(t => Source.tick(t, t, StateUpd(SPState("tick", Map()), List()))).getOrElse(Source.empty[StateUpd])
     Flow[StateUpd]
       .merge(ticker)
-      .ask[RunnerLogic.OneOperationRun](1)(runnerA)
+      .ask[RunnerLogic.OneOperationRun](2)(runnerA)
       .mapConcat(x =>
         x.sequence.map(_._2).reverse
       )
@@ -39,6 +39,7 @@ case class RunnerPipeline(operations: List[Operation],
   def getRunnerData: Future[SPAttributes] = {
     val res = (runnerA ? GetRunnerData).mapTo[RunnerState]
 
+    // Make a better API with case classes in API. Do not leak RunnerActorAPI though
     res.map(i =>
       SPAttributes(
         "operations" -> i.ops,
@@ -54,11 +55,9 @@ case class RunnerPipeline(operations: List[Operation],
   }
 
 
-// Change this so the actor is doing the merge and not like this since we can have
-// a race condition where this method overwrites a new state
-
+//
 //  def addOperations(ops: List[Operation], initialState: SPState) = {
-//    (runnerA ? GetRunnerData).mapTo[RunnerState].map{rs =>
+//    (runnerA ? AddOps(ops, initialState)).mapTo[RunnerState].map{rs =>
 //      val newOps = rs.ops ++ ops distinct
 //      val newS = rs.state.next(initialState.state)
 //      runnerA ! SetRunnerData()
@@ -87,14 +86,25 @@ trait RunnerActorAPI
 case object GetRunnerData extends RunnerActorAPI
 case class MakeControlled(ts: List[ID]) extends RunnerActorAPI
 case class MakeUnControlled(ts: List[ID]) extends RunnerActorAPI
-case class SetRunnerData(data: RunnerState) extends RunnerActorAPI
+case class RemoveFromState(ids: List[ID]) extends RunnerActorAPI
+case class AddOps(ops: List[Operation], initialState: SPState) extends RunnerActorAPI
+case class RemoveOps(ops: List[ID]) extends RunnerActorAPI
+case class AddDisabledGroups(disabledGroups: List[SPValue]) extends RunnerActorAPI
+case class RemoveDisabledGroups(disabledGroups: List[SPValue]) extends RunnerActorAPI
+case class AddPersistentEvents(persistentEvents: List[RunnerLogic.FireEvent]) extends RunnerActorAPI
+case class RemovePersistentEvents(persistentEvents: List[RunnerLogic.FireEvent]) extends RunnerActorAPI
+
+
+
+
+
 case class StateUpd(s: SPState, events: List[RunnerLogic.FireEvent]) extends RunnerActorAPI
 object StateUpd {
   def empty = StateUpd(SPState("empty", Map()), List())
 }
 
 
-case class RunnerState(state: SPState,
+case class RunnerState(state: Option[SPState],
                        ops: List[Operation],
                        controlledTransitions: List[RunnerLogic.OperationTransition],
                        unControlledTransitions: List[RunnerLogic.OperationTransition],
@@ -110,23 +120,24 @@ class RunnerActor(transitionSystem: List[RunnerLogic.OperationTransition],
 
 
   var internal = RunnerState(
-    state = initialState,
+    state = None,
     ops = operations,
     controlledTransitions = transitionSystem.filter(_.event.nonEmpty),
     unControlledTransitions = transitionSystem.filter(_.event.isEmpty),
     persistentEvents = List(),
     disabledGroups = Set()
   )
+  var state = initialState
 
 
 
 
   override def receive = {
-    case StateUpd(s, es) =>
-      println("Tjo")
-      println(internal)
+    case StateUpd(s, es) =>  // Only allowed to be called via the flow!
+      //println("Tjo")
+      //println(internal)
 
-      val updS = internal.state.next(s.state)
+      val updS = state.next(s.state)
       val res = RunnerLogic.runOperations(
         internal.ops,
         updS,
@@ -135,19 +146,33 @@ class RunnerActor(transitionSystem: List[RunnerLogic.OperationTransition],
         internal.unControlledTransitions,
         internal.disabledGroups
       )
-      internal = internal.copy(state = res.lastState)
+      state = res.lastState
       sender() ! res
 
-    case GetRunnerData => sender() ! internal
-      // change so we do merge here instead
-//    case SetRunnerData(d) =>
-//      internal = internal.copy(
-//        ops = if (d.ops.nonEmpty) d.ops else internal.ops,
-//        persistentEvents = if (d.persistentEvents.nonEmpty) d.persistentEvents else internal.persistentEvents,
-//        disabledGroups = if (d.disabledGroups.nonEmpty) d.disabledGroups else internal.disabledGroups,
-//        controlledTransitions = if (d.controlledTransitions.nonEmpty) d.controlledTransitions else internal.controlledTransitions,
-//        unControlledTransitions = if (d.unControlledTransitions.nonEmpty) d.unControlledTransitions else internal.unControlledTransitions
-//      )
+
+    case GetRunnerData => sender() ! theState()
+    case RemoveFromState(xs) =>
+      state = state.copy(state = state.state.filter(kv => !xs.contains(kv._1)))
+      sender() ! theState()
+    case AddOps(xs, s) =>
+      internal = internal.copy(ops = (internal.ops ++ xs).distinct)
+      state = state.next(s.state)
+      sender() ! theState()
+    case RemoveOps(xs) =>
+      internal = internal.copy(ops = internal.ops.filter(o => !xs.contains(o.id)))
+      state = state.copy(state = state.state.filter(kv => !xs.contains(kv._1)))
+      sender() ! theState()
+    case AddDisabledGroups(xs) =>
+      internal = internal.copy(disabledGroups = internal.disabledGroups ++ xs)
+      sender() ! theState()
+    case RemoveDisabledGroups(xs) =>
+      internal = internal.copy(disabledGroups = internal.disabledGroups.filter(x => !xs.contains(x)))
+      sender() ! theState()
+    case AddPersistentEvents(xs) =>
+      internal = internal.copy(persistentEvents = (internal.persistentEvents ++ xs).distinct)
+    case RemovePersistentEvents(xs) =>
+        internal = internal.copy(persistentEvents = internal.persistentEvents.filter(x => !xs.contains(x)))
+        sender() ! theState()
     case MakeControlled(ts) =>
       val xs = transitionSystem.filter(t => ts.contains(t.id))
       val unc = internal.unControlledTransitions.filter(xs.contains)
@@ -156,6 +181,7 @@ class RunnerActor(transitionSystem: List[RunnerLogic.OperationTransition],
         controlledTransitions = c,
         unControlledTransitions = unc
       )
+      sender() ! theState()
     case MakeUnControlled(ts) =>
       val xs = transitionSystem.filter(t => ts.contains(t.id))
       val c = internal.controlledTransitions.filter(xs.contains)
@@ -164,15 +190,18 @@ class RunnerActor(transitionSystem: List[RunnerLogic.OperationTransition],
         controlledTransitions = c,
         unControlledTransitions = unc
       )
+      sender() ! theState()
 
   }
 
+  def theState() = internal.copy(state = Some(state))
 
-  private def validateState = {
-    val opsState = internal.ops.map(o => o.id -> internal.state.get(o.id))
-    if (opsState.exists(_._2.isEmpty)){
-      internal = internal.copy(state = internal.state.next(opsState.map(kv => kv._1 -> kv._2.getOrElse(SPValue(""))).toMap))
-    }
-  }
+
+//  private def validateState = {
+//    val opsState = internal.ops.map(o => o.id -> state.get(o.id))
+//    if (opsState.exists(_._2.isEmpty)){
+//      internal = internal.copy(state = state.next(opsState.map(kv => kv._1 -> kv._2.getOrElse(SPValue(""))).toMap))
+//    }
+//  }
 
 }
