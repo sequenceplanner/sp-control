80142e4476380183c16d5c0d0d92d09aa9d5cc28
aleastCHS <aleast@student.chalmers.se>
2019-01-07 16:47:51 +0100

    Changed ModelChoiceDropdown to use new Buttons

--- frontend/src/main/scala/spgui/widgets/itemexplorerincontrol/ModelChoiceDropdown.scala
+++ frontend/src/main/scala/spgui/widgets/itemexplorerincontrol/ModelChoiceDropdown.scala
@@ -5,7 +5,8 @@ import japgolly.scalajs.react.vdom.html_<^._
 import japgolly.scalajs.react._
 import sp.domain.ID
 import spgui.circuits.availablemodelscircuit.{AvailableModels, AvailableModelsCircuit}
-import spgui.components.SPWidgetElements
+import spgui.components.Button._
+import spgui.components.Icon
 
 object ModelChoiceDropdown {
   case class Props(proxy: ModelProxy[AvailableModels], cb: ID => Callback)
@@ -14,8 +15,8 @@ object ModelChoiceDropdown {
 
   val component = ScalaComponent.builder[Props]("ModelChoiceDropdown")
     .render_P { p =>
-      val contents = p.proxy().models.toList.map(kv => <.div(kv._2, ^.onClick --> p.cb(kv._1)))
-      SPWidgetElements.dropdown("Choose Model", contents)
+      val contents = p.proxy().models.toList.map(kv => SPDropdownItem(kv._2, p.cb(kv._1)))
+      SPDropdown("Choose Model", Callback.empty, Icon.arrowDown, contents)
     }
     .build
 
